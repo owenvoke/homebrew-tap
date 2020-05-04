c5f903f06e7d6a84920570367905e7296e8cff70
Owen Voke <development@voke.dev>
2020-05-04 16:23:29 +0100

    helo: Update to version 1.1.1

--- Casks/helo.rb
+++ Casks/helo.rb
@@ -1,6 +1,6 @@
 cask 'helo' do
-  version '1.1.0'
-  sha256 'a3989ce2e52c1614d729f7b8dcfcf5668292b459f16fd116064732389eeb19bb'
+  version '1.1.1'
+  sha256 'b1fe270ed460685f5c6825a422c7dece61350fc470ce6577f13346236eb9d174'
 
   # helo.fra1.digitaloceanspaces.com was verified as official when first introduced to the cask
   url "https://helo.fra1.digitaloceanspaces.com/helo/HELO-#{version}.dmg"
@@ -8,9 +8,9 @@ cask 'helo' do
   name 'HELO'
   homepage 'https://usehelo.com/'
 
-  app 'HELO.app'
-
   auto_updates true
+
+  app 'HELO.app'
 
   zap trash: [
                '~/Library/Application Support/HELO',
