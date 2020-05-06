1c369b3c1d3a42b295f71733f7077339a6b9051c
Owen Voke <development@voke.dev>
2020-05-06 10:37:45 +0100

    Add Tea v0.3.0

---
+++ Formula/tea.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Tea < Formula
+  homepage 'https://gitea.com/gitea/tea'
+  version '0.3.0'
+
+  url "https://dl.gitea.io/tea/#{version}/tea-#{version}-darwin-10.6-amd64"
+  sha256 '185aee306028715aea58f45888af0685173c01a1d4a30e0c9bccc4bf854f7a6b'
+
+  def install
+    bin.install "tea-#{version}-darwin-10.6-amd64" => 'tea'
+  end
+end
