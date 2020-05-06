414f5beaad6dda591ed973793765aeff57aa3f68
Owen Voke <development@voke.dev>
2020-05-06 10:53:07 +0100

    tea: Add 'bottle :unneeded' stanza

--- Formula/tea.rb
+++ Formula/tea.rb
@@ -7,6 +7,8 @@ class Tea < Formula
   url "https://dl.gitea.io/tea/#{version}/tea-#{version}-darwin-10.6-amd64"
   sha256 '185aee306028715aea58f45888af0685173c01a1d4a30e0c9bccc4bf854f7a6b'
 
+  bottle :unneeded
+
   def install
     bin.install "tea-#{version}-darwin-10.6-amd64" => 'tea'
   end
