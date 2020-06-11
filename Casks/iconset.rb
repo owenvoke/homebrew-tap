c16f484a74e0ef91d11738590b36cb4a87322c53
Owen Voke <development@voke.dev>
2020-06-11 17:48:56 +0100

    iconset: Update to version 1.3.11

---
+++ Casks/iconset.rb
@@ -0,0 +1,12 @@
+cask 'iconset' do
+  version '1.3.11'
+  sha256 '33b86a855464f16dcbbd9fcde1cbb2eeb4a20d7aecc1b68393a82d54bb1bbac4'
+
+  # github.com was verified as official when first introduced to the cask
+  url "https://github.com/IconsetApp/iconset/releases/download/v#{version}/Iconset-#{version}-mac.zip"
+  appcast 'https://iconset.io/changelog'
+  name 'Iconset'
+  homepage 'https://iconset.io/'
+
+  app 'Iconset.app'
+end
