c50e3bda905d3afe486d396d57f63260cb244378
Owen Voke <development@voke.dev>
2020-08-17 14:59:14 +0100

    tea: update to version 0.4.0

--- Formula/tea.rb
+++ Formula/tea.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Tea < Formula
   homepage 'https://gitea.com/gitea/tea'
-  version '0.3.1'
+  version '0.4.0'
 
   url "https://dl.gitea.io/tea/#{version}/tea-#{version}-darwin-10.6-amd64"
-  sha256 'a9d1881bbd642c414b2f7177bfed1a6b560fdaa8ee791add38d35317c8973728'
+  sha256 '9d8bebec8a67abd6ce060358bb1a63ca4dc47953104f653a5ba66f6522084224'
 
   bottle :unneeded
 
