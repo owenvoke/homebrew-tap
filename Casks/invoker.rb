9ee4b4f2198ffc870e852671af3a447b266b8876
Owen Voke <development@voke.dev>
2020-09-15 15:52:08 +0100

    invoker: update to version 2.0.0

---
+++ Casks/invoker.rb
@@ -0,0 +1,21 @@
+cask 'invoker' do
+  version '2.0.0'
+  sha256 '30a5aa3071db7a1a55f60c6003f55eba429a9dc3270e426683b556e243f493c2'
+
+  # invoker.ams3.digitaloceanspaces.com was verified as official when first introduced to the cask
+  url "https://invoker.ams3.digitaloceanspaces.com/invoker/Invoker-#{version}.dmg"
+  appcast 'https://invoker.ams3.digitaloceanspaces.com/invoker/latest-mac.yml'
+  name 'Invoker'
+  homepage 'https://invoker.dev/'
+
+  auto_updates true
+
+  app 'Invoker.app'
+
+  zap trash: [
+               '~/Library/Application Support/Invoker',
+               '~/Library/Logs/Invoker',
+               '~/Library/Preferences/de.beyondco.invoker.plist',
+               '~/Library/Saved Application State/de.beyondco.invoker.savedState',
+             ]
+end
