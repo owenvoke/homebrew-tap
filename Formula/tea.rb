0d210d633e0749d412af47af216ef7bfc87a1472
Owen Voke <development@voke.dev>
2020-09-28 09:59:57 +0100

    tea: cleanup spacing

--- Formula/tea.rb
+++ Formula/tea.rb
@@ -3,7 +3,6 @@ class Tea < Formula
   homepage "https://gitea.com/gitea/tea"
 
   url "https://dl.gitea.io/tea/0.5.0/tea-0.5.0-darwin-10.6-amd64"
-
   sha256 "f51d4d5c4aefb4a37d904d215c5c5ddd8b3d209e0af9d40e2c4a5e717b258a53"
 
   bottle :unneeded
