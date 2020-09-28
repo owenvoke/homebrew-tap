572d60724ddcf30b87aac96b91570d0d55d654fb
Owen Voke <development@voke.dev>
2020-09-28 09:44:36 +0100

    tea: update to version 0.5.0

--- Formula/tea.rb
+++ Formula/tea.rb
@@ -1,15 +1,19 @@
-require 'formula'
-
 class Tea < Formula
-  homepage 'https://gitea.com/gitea/tea'
-  version '0.4.0'
+  desc "Command-line tool to interact with Gitea servers"
+  homepage "https://gitea.com/gitea/tea"
 
   url "https://dl.gitea.io/tea/#{version}/tea-#{version}-darwin-10.6-amd64"
-  sha256 '9d8bebec8a67abd6ce060358bb1a63ca4dc47953104f653a5ba66f6522084224'
+  version "0.5.0"
+
+  sha256 "f51d4d5c4aefb4a37d904d215c5c5ddd8b3d209e0af9d40e2c4a5e717b258a53"
 
   bottle :unneeded
 
   def install
-    bin.install "tea-#{version}-darwin-10.6-amd64" => 'tea'
+    bin.install "tea-#{version}-darwin-10.6-amd64" => "tea"
+  end
+
+  test do
+    system "#{bin}/tea", "--version"
   end
 end
