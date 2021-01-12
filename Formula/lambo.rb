8fc0d2c9fc16ab47a51f4706b9426591c859cf29
Owen Voke <development@voke.dev>
2021-01-12 14:18:00 +0000

    chore(lambo): update to version 1.0.0beta5

---
+++ Formula/lambo.rb
@@ -0,0 +1,19 @@
+class Lambo < Formula
+  desc "Super-powered `laravel new` for Laravel and Valet"
+  homepage "https://gitea.com/tighten/lambo"
+  url "https://github.com/tighten/lambo/raw/v1.0.0beta5/builds/lambo"
+  sha256 "6d92e1fa2d00f4528129ddc51e04bd2885e9d652090db4187c784ece6a3f0e9b"
+  license "MIT"
+
+  bottle :unneeded
+
+  depends_on "php"
+
+  def install
+    bin.install "lambo"
+  end
+
+  test do
+    system "#{bin}/lambo", "--version"
+  end
+end
