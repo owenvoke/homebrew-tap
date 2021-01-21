e57d543a8d05babd4d26681213d3b24199b42bad
Owen Voke <development@voke.dev>
2021-01-21 12:34:09 +0000

    fix: update links

--- Formula/lambo.rb
+++ Formula/lambo.rb
@@ -1,6 +1,6 @@
 class Lambo < Formula
   desc "Super-powered `laravel new` for Laravel and Valet"
-  homepage "https://gitea.com/tighten/lambo"
+  homepage "https://github.com/tighten/lambo"
   url "https://github.com/tighten/lambo/raw/v1.0.0beta5/builds/lambo"
   sha256 "6d92e1fa2d00f4528129ddc51e04bd2885e9d652090db4187c784ece6a3f0e9b"
   license "MIT"
--- Formula/takeout.rb
+++ Formula/takeout.rb
@@ -1,6 +1,6 @@
 class Takeout < Formula
   desc "Docker-based development-only dependency manager"
-  homepage "https://gitea.com/tighten/takeout"
+  homepage "https://github.com/tighten/takeout"
   url "https://github.com/tighten/takeout/raw/v1.8.2/builds/takeout"
   sha256 "9973978f9d4ab2f87cbfa2dfbb75e67b5bcd9459a36779d7934d1eaecea25f1b"
   license "MIT"
