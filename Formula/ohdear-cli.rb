f295597313c464520f0ff4b3c93b57dc6cb28392
Owen Voke <development@voke.dev>
2021-01-22 13:24:09 +0000

    chore(ohdear-cli): update to version 2.6.0

--- Formula/ohdear-cli.rb
+++ Formula/ohdear-cli.rb
@@ -1,8 +1,8 @@
 class OhdearCli < Formula
   desc "Tool to manage your Oh Dear sites"
   homepage "https://github.com/nunomaduro/ohdear-cli"
-  url "https://github.com/nunomaduro/ohdear-cli/releases/download/v2.5.0/ohdear-cli.phar"
-  sha256 "9ff436a103c48346f478d5e96e66cf7648e111df5d9d5aeb04347cad0f8bced2"
+  url "https://github.com/nunomaduro/ohdear-cli/releases/download/v2.6.0/ohdear-cli.phar"
+  sha256 "6526e0f0f4d23bbf111f53a6c7b4e6980651353186e6e9bbbb459512fbc2e238"
   license "MIT"
 
   livecheck do
