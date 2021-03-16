4ff05febaad6cac21e448140fd0787dacd1cee73
Owen Voke <development@voke.dev>
2021-03-16 13:13:02 +0000

    chore(tea): update to version 0.7.0

--- Formula/tea.rb
+++ Formula/tea.rb
@@ -2,7 +2,7 @@ class Tea < Formula
   desc "Command-line tool to interact with Gitea servers"
   homepage "https://gitea.com/gitea/tea"
   url "https://dl.gitea.io/tea/0.6.0/tea-0.6.0-darwin-amd64"
-  version "0.6.0"
+  version "0.7.0"
   sha256 "8d2e61d7f8ef92f9198f903cf99dc889468a710466397244993cb5218be2276c"
 
   bottle :unneeded
