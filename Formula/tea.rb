50bd457a2e02a5fa45c79a32c5437a4ba8550fc0
Owen Voke <development@voke.dev>
2021-03-16 13:19:18 +0000

    fix(tea): update link

--- Formula/tea.rb
+++ Formula/tea.rb
@@ -1,9 +1,9 @@
 class Tea < Formula
   desc "Command-line tool to interact with Gitea servers"
   homepage "https://gitea.com/gitea/tea"
-  url "https://dl.gitea.io/tea/0.6.0/tea-0.6.0-darwin-amd64"
+  url "https://dl.gitea.io/tea/0.7.0/tea-0.7.0-darwin-amd64"
   version "0.7.0"
-  sha256 "8d2e61d7f8ef92f9198f903cf99dc889468a710466397244993cb5218be2276c"
+  sha256 "20a350fb8583ee7e33f133d73caf74a789805995e999e6ebd8fd09c0383e0158"
 
   bottle :unneeded
 
