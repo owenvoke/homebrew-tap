33b2b918657a871613ff23662ef53b700d6a3639
Owen Voke <development@voke.dev>
2021-03-16 13:12:48 +0000

    chore(lambo): update to version 1.0.0

--- Formula/lambo.rb
+++ Formula/lambo.rb
@@ -1,14 +1,11 @@
 class Lambo < Formula
   desc "Super-powered `laravel new` for Laravel and Valet"
   homepage "https://github.com/tighten/lambo"
-  url "https://github.com/tighten/lambo/raw/v1.0.0beta5/builds/lambo"
+  url "https://github.com/tighten/lambo/raw/v1.0.0/builds/lambo"
   sha256 "6d92e1fa2d00f4528129ddc51e04bd2885e9d652090db4187c784ece6a3f0e9b"
   license "MIT"
 
   livecheck do
-    # Remove once stable
-    skip "This application is not stable"
-
     url :stable
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
