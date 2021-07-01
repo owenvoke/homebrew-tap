2f5357e00243cf13f77c6c775074f9dac13458a3
Owen Voke <development@voke.dev>
2021-07-01 12:25:14 +0100

    chore(laravelshift-workbench): update to version 0.2.1

---
+++ Casks/laravelshift-workbench.rb
@@ -0,0 +1,29 @@
+cask "laravelshift-workbench" do
+  version "0.2.1"
+  sha256 "6eea3057434057a38df43c6dd4f46f17dabcc3ec4e3e7218b9c834d4be10a266"
+
+  if Hardware::CPU.intel?
+    url "https://shift-cloud.s3.amazonaws.com/workbench/Workbench-#{version}.dmg",
+        verified: "https://shift-cloud.s3.amazonaws.com/workbench/"
+  else
+    url "https://shift-cloud.s3.amazonaws.com/workbench/Workbench-#{version}-arm64.dmg",
+        verified: "https://shift-cloud.s3.amazonaws.com/workbench/"
+  end
+
+  name "Laravel Shift Workbench"
+  desc "Upgrade and manage Laravel applications"
+  homepage "https://laravelshift.com/workbench"
+
+  livecheck do
+    url "https://laravelshift.com/workbench/download/mac-intel"
+    strategy :header_match
+  end
+
+  app "Workbench.app"
+
+  zap trash: [
+    "~/Library/Application Support/Workbench",
+    "~/Library/Preferences/com.laravelshift.workbench.plist",
+    "~/Library/Saved Application State/com.laravelshift.workbench.savedState",
+  ]
+end
