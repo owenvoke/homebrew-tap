d5ee1b8e950ed8672194f7ed5a76dcca04e720c2
Owen Voke <development@voke.dev>
2021-08-24 10:40:26 +0100

    takeout 1.8.11

--- Formula/takeout.rb
+++ Formula/takeout.rb
@@ -1,8 +1,8 @@
 class Takeout < Formula
   desc "Docker-based development-only dependency manager"
   homepage "https://github.com/tighten/takeout"
-  url "https://github.com/tighten/takeout/raw/v1.8.10/builds/takeout"
-  sha256 "8555cb1702bc1c48929364d378421dc132c0e5144d6ad06c6877d5f389e4f530"
+  url "https://github.com/tighten/takeout/raw/v1.8.11/builds/takeout"
+  sha256 "2052ff212a40e5895fcfddc4678a38421658fd8e4fd173b802f0b79e8794e018"
   license "MIT"
 
   livecheck do
