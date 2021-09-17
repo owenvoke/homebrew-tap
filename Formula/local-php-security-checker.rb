2e6660ff3008f390c14f74e47a570362d0e4a645
Owen Voke <development@voke.dev>
2021-09-17 13:13:28 +0100

    chore(local-php-security-checker): update to version 1.1.1 (#20)

--- Formula/local-php-security-checker.rb
+++ Formula/local-php-security-checker.rb
@@ -1,9 +1,9 @@
 class LocalPhpSecurityChecker < Formula
   desc "Check if your PHP application uses packages with known security vulnerabilities"
   homepage "https://github.com/fabpot/local-php-security-checker"
-  url "https://github.com/fabpot/local-php-security-checker/releases/download/v1.0.0/local-php-security-checker_1.0.0_darwin_amd64"
-  version "1.0.0"
-  sha256 "8c363d605116e84cf9ac28ac3838ca7979f7306916049bdb3f0f1fe2a8764d82"
+  url "https://github.com/fabpot/local-php-security-checker/releases/download/v1.1.1/local-php-security-checker_1.1.1_darwin_amd64"
+  version "1.1.1"
+  sha256 "633078c0816b670ff9c55712b7f4229279d9eb0cd54ed519a57c26b3529aaeef"
 
   livecheck do
     url :stable
