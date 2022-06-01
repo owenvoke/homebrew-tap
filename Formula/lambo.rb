f7fbf606b8251e796ab9b78a75bd6b0dffb612bd
Owen Voke <development@voke.dev>
2022-06-01 10:09:31 +0100

    chore(lambo): update to version 2.0.1 (#29)

--- Formula/lambo.rb
+++ Formula/lambo.rb
@@ -1,7 +1,7 @@
 class Lambo < Formula
   desc "Super-powered `laravel new` for Laravel and Valet"
   homepage "https://github.com/tighten/lambo"
-  url "https://github.com/tighten/lambo/raw/v2.0.0/builds/lambo"
+  url "https://github.com/tighten/lambo/raw/2.0.1/builds/lambo"
   sha256 "fb5e4a6f87239bd5692a7314ebf2542c4f14d2806126a1b6907b8393c65a4341"
   license "MIT"
 
