07a37595690caaf0d654fbe8690202e4a48e066e
Owen Voke <development@voke.dev>
2022-06-14 15:39:11 +0100

    chore(graphlint): update to version 0.2.1

---
+++ Formula/graphlint.rb
@@ -0,0 +1,18 @@
+class Graphlint < Formula
+  desc "Static analysis tool for GraphQL"
+  homepage "https://github.com/worksome/graphlint"
+  url "https://github.com/worksome/graphlint/releases/download/v0.2.1/graphlint"
+  sha256 "4ae8effbc0f090e2b52ea0c4427b458ca5b29fd616d82f398d568f6a993685d5"
+  license "MIT"
+
+  depends_on "php"
+
+  def install
+    bin.install "graphlint"
+  end
+
+  test do
+    assert_match "Unable to find a \"graphlint.php\" configuration file",
+                 shell_output("#{bin}/graphlint test.graphql", 1)
+  end
+end
