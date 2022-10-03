e453069bbb378a2ee8f041c0cd650cf572c3d3b5
Owen Voke <development@voke.dev>
2022-10-03 09:05:29 +0100

    Update laravelshift-workbench from 1.0.1 to 1.0.3

--- Casks/laravelshift-workbench.rb
+++ Casks/laravelshift-workbench.rb
@@ -1,13 +1,14 @@
 cask "laravelshift-workbench" do
-  version "1.0.1"
+  version "1.0.3"
 
-  if Hardware::CPU.intel?
+  on_intel do
     sha256 "c45598ddb972caf95ab7a1625647e04afb9c93bfe0172b71ef50387955ce08f6"
 
     url "https://shift-cloud.s3.amazonaws.com/workbench/Workbench-#{version}.dmg",
         verified: "https://shift-cloud.s3.amazonaws.com/workbench/"
-  else
-    sha256 "e60e10b77b35bf61e0ba87edc0ec76aca15913b8f746c714d3d2a3d4b9d73de1"
+  end
+  on_arm do
+    sha256 "e48beb2989d4f0a1e28dfe1cb9794653ed14258b433f58cababb5c4eb87cdda2"
 
     url "https://shift-cloud.s3.amazonaws.com/workbench/Workbench-#{version}-arm64.dmg",
         verified: "https://shift-cloud.s3.amazonaws.com/workbench/"
