baa364052614449943a0a6a21e765355ec26050f
Owen Voke <development@voke.dev>
2022-10-03 09:07:59 +0100

    chore(laravelshift-workbench): fix hash for Intel

--- Casks/laravelshift-workbench.rb
+++ Casks/laravelshift-workbench.rb
@@ -2,11 +2,12 @@ cask "laravelshift-workbench" do
   version "1.0.3"
 
   on_intel do
-    sha256 "c45598ddb972caf95ab7a1625647e04afb9c93bfe0172b71ef50387955ce08f6"
+    sha256 "7612ee92450b45be1b59b7129430ed74ec0a4ed9036f20f19235ef1a5237cb66"
 
     url "https://shift-cloud.s3.amazonaws.com/workbench/Workbench-#{version}.dmg",
         verified: "https://shift-cloud.s3.amazonaws.com/workbench/"
   end
+
   on_arm do
     sha256 "e48beb2989d4f0a1e28dfe1cb9794653ed14258b433f58cababb5c4eb87cdda2"
 
