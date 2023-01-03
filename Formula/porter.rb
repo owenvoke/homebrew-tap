6dbdf85431f24ee61f3848e7771c0fe9b2fe87d0
Owen Voke <development@voke.dev>
2023-01-03 13:35:55 +0000

    feat(porter): add Porter

---
+++ Formula/porter.rb
@@ -0,0 +1,22 @@
+class Porter < Formula
+  desc "Docker-based development-only dependency manager"
+  homepage "https://github.com/anystack-sh/porter"
+  url "https://github.com/anystack-sh/porter/raw/0.0.4/builds/porter"
+  sha256 "55f34f4d722827d4bb9c0c8c578f2b85c8ac116f01a2f07548b6000e6e725594"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "php"
+
+  def install
+    bin.install "porter"
+  end
+
+  test do
+    system "#{bin}/porter", "--version"
+  end
+end
