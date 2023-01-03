a0e46bd45c7f65bf3115d4eede5083b23d949fa7
Owen Voke <development@voke.dev>
2023-01-03 13:43:17 +0000

    chore(takeout): update to version 2.3.0 (#47)

--- Formula/takeout.rb
+++ Formula/takeout.rb
@@ -1,8 +1,8 @@
 class Takeout < Formula
   desc "Docker-based development-only dependency manager"
   homepage "https://github.com/tighten/takeout"
-  url "https://github.com/tighten/takeout/raw/v2.1.1/builds/takeout"
-  sha256 "57a0da4ec84820a9790ee7676ad076303cd57f6fdbe4b76f30bebeef22ba1357"
+  url "https://github.com/tighten/takeout/raw/v2.3.0/builds/takeout"
+  sha256 "ee60c1d6cc3786b92617e8ea9cc66a64b5f33ac3d6a01251f12e0807b3fd1d0c"
   license "MIT"
 
   livecheck do
