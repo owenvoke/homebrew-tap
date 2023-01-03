a2b6bf52f3154b2c84b10be04f7378d16e7de4c1
Owen Voke <development@voke.dev>
2023-01-03 13:41:55 +0000

    local-php-security-checker 2.0.6

--- Formula/local-php-security-checker.rb
+++ Formula/local-php-security-checker.rb
@@ -1,9 +1,9 @@
 class LocalPhpSecurityChecker < Formula
   desc "Check if your PHP application uses packages with known security vulnerabilities"
   homepage "https://github.com/fabpot/local-php-security-checker"
-  url "https://github.com/fabpot/local-php-security-checker/releases/download/v2.0.5/local-php-security-checker_2.0.5_darwin_amd64"
-  version "2.0.5"
-  sha256 "072fc83cffd50c42432cd5c895113652ff64f25c193bc60719dcd63a2aa9fbad"
+  url "https://github.com/fabpot/local-php-security-checker/releases/download/v2.0.6/local-php-security-checker_2.0.6_darwin_amd64"
+  version "2.0.6"
+  sha256 "d7bd8154d76ace5a1e9d7f6af3176714830971b5af2ca81f1b63576a885e07b5"
 
   livecheck do
     url :stable
