09938b3acce6de0dc225d03ec314c517483a91c6
Owen Voke <development@voke.dev>
2023-01-24 14:40:26 +0000

    fix: resolve Porter description

--- Formula/porter.rb
+++ Formula/porter.rb
@@ -1,5 +1,5 @@
 class Porter < Formula
-  desc "Docker-based development-only dependency manager"
+  desc "Background service manager"
   homepage "https://github.com/anystack-sh/porter"
   url "https://github.com/anystack-sh/porter/raw/0.0.4/builds/porter"
   sha256 "55f34f4d722827d4bb9c0c8c578f2b85c8ac116f01a2f07548b6000e6e725594"
