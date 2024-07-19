e0eda48900aa0ee2b24e2f8194171736b96548a2
Owen Voke <development@voke.dev>
2024-07-19 16:40:33 +0100

    style(laravelshift-workbench): reformat cask

--- Casks/laravelshift-workbench.rb
+++ Casks/laravelshift-workbench.rb
@@ -1,17 +1,16 @@
 cask "laravelshift-workbench" do
   version "1.0.3"
 
-  on_intel do
-    sha256 "7612ee92450b45be1b59b7129430ed74ec0a4ed9036f20f19235ef1a5237cb66"
-
-    url "https://shift-cloud.s3.amazonaws.com/workbench/Workbench-#{version}.dmg",
-        verified: "https://shift-cloud.s3.amazonaws.com/workbench/"
-  end
-
   on_arm do
     sha256 "e48beb2989d4f0a1e28dfe1cb9794653ed14258b433f58cababb5c4eb87cdda2"
 
     url "https://shift-cloud.s3.amazonaws.com/workbench/Workbench-#{version}-arm64.dmg",
+        verified: "https://shift-cloud.s3.amazonaws.com/workbench/"
+  end
+  on_intel do
+    sha256 "7612ee92450b45be1b59b7129430ed74ec0a4ed9036f20f19235ef1a5237cb66"
+
+    url "https://shift-cloud.s3.amazonaws.com/workbench/Workbench-#{version}.dmg",
         verified: "https://shift-cloud.s3.amazonaws.com/workbench/"
   end
 
