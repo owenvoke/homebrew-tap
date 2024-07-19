bc0dee90215cf29ba99bf4a8555b62c591f99e71
Owen Voke <development@voke.dev>
2024-07-19 16:10:26 +0100

    chore(local-php-security-checker): update to version 2.1.3

--- Formula/local-php-security-checker.rb
+++ Formula/local-php-security-checker.rb
@@ -1,17 +1,28 @@
 class LocalPhpSecurityChecker < Formula
   desc "Check if your PHP application uses packages with known security vulnerabilities"
   homepage "https://github.com/fabpot/local-php-security-checker"
-  url "https://github.com/fabpot/local-php-security-checker/releases/download/v2.0.6/local-php-security-checker_2.0.6_darwin_amd64"
-  version "2.0.6"
-  sha256 "d7bd8154d76ace5a1e9d7f6af3176714830971b5af2ca81f1b63576a885e07b5"
+  version "2.1.3"
 
   livecheck do
     url :stable
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  if Hardware::CPU.arm?
+    sha256 "31753d681c0c67a673aac71c68da18401fe87aa8cf2097f98e91696e127a8186"
+    url "https://github.com/fabpot/local-php-security-checker/releases/download/v2.1.3/local-php-security-checker_darwin_arm64",
+        verified: "https://github.com/fabpot/local-php-security-checker/releases/"
+  end
+
+  if Hardware::CPU.intel?
+    sha256 "4734f48369f60012e408cb83af0f7bb15f1cbb3f626cf26a38c3434e610247b0"
+    url "https://github.com/fabpot/local-php-security-checker/releases/download/v2.1.3/local-php-security-checker_darwin_amd64",
+        verified: "https://github.com/fabpot/local-php-security-checker/releases/"
+  end
+
   def install
-    bin.install "local-php-security-checker_#{version}_darwin_amd64" => "local-php-security-checker"
+    bin.install "local-php-security-checker_darwin_amd64" => "local-php-security-checker" if Hardware::CPU.intel?
+    bin.install "local-php-security-checker_darwin_arm64" => "local-php-security-checker" if Hardware::CPU.arm?
   end
 
   test do
--- Formula/noscl.rb
+++ Formula/noscl.rb
@@ -1,7 +1,7 @@
 class Noscl < Formula
   desc "Command-line client for Nostr"
   homepage "https://github.com/fiatjaf/noscl"
-  url "https://github.com/fiatjaf/noscl/archive/v0.6.0.tar.gz"
+  url "https://github.com/fiatjaf/noscl/archive/refs/tags/v0.6.0.tar.gz"
   sha256 "4cabb22ca6e739b89003f79ae7bd17d3197802456b8395f5ddde4e412bffa2a2"
   license :public_domain
   head "https://github.com/fiatjaf/noscl.git", branch: "master"
