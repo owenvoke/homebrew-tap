d0a5cea5ade9a5c6eedad67fc33a3e7453cfe289
Owen Voke <development@voke.dev>
2025-06-20 15:34:06 +0100

    rugby-schedule 1.13.0 (#63)

--- Formula/rugby-schedule.rb
+++ Formula/rugby-schedule.rb
@@ -1,8 +1,8 @@
 class RugbySchedule < Formula
   desc "Keep up to date with rugby tournament schedules"
   homepage "https://github.com/owenvoke/rugby-schedule"
-  url "https://github.com/owenvoke/rugby-schedule/raw/v1.12.0/builds/rugby-schedule"
-  sha256 "7b33c6278b89ec96848b9d5efef250a22def86fdefc1cc2a4def1ba50677d2c8"
+  url "https://github.com/owenvoke/rugby-schedule/raw/v1.13.0/builds/rugby-schedule"
+  sha256 "894da30dbf02e9377c7d540da25038b3fa0d489cbc94e189f589f247b6fcd811"
   license "MIT"
 
   livecheck do
